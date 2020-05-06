aafc6d8a84968454e40b0a19741a6bb9dcec81a0
ctufaro <ctufaro@gmail.com>
2020-05-05 20:19:58 -0400

    Beans

--- KikShowAPI/Controllers/UserPostController.cs
+++ KikShowAPI/Controllers/UserPostController.cs
@@ -28,9 +28,33 @@ namespace KikShowAPI.Controllers
 
         // POST: api/UserPost
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ObjectResult> Post([FromForm]UserPostData userPD)
         {
+            int userId = userPD.UserId;
+            string title = userPD.Title;
+            var motion = userPD.Motion;
+            var image = userPD.Image;
+            if (motion.Length > 0)
+            {
+                Storage storage = new Storage();
+                using (var stream = motion.OpenReadStream())
+                {
+                    //uploading motion to azure
+                    await storage.UploadToStorage(stream, motion.FileName);
+                }
+            }
+            if (image.Length > 0)
+            {
+                Storage storage = new Storage();
+                using (var stream = image.OpenReadStream())
+                {
+                    //uploading motion to azure
+                    await storage.UploadToStorage(stream, image.FileName);
+                }
+            }
+            return Ok(new { status = true, message = "User Video Posted Successfully!!" });
         }
+
 
         // PUT: api/UserPost/5
         [HttpPut("{id}")]
---
+++ KikShowAPI/Models/UserPostData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KikShowAPI.Models
+{
+    public class UserPostData
+    {
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public IFormFile Motion { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
